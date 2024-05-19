9ea90f00608f37de2c85af42a46cd62e0d5978a2
Tanner Johnson <tanner.johnson2718@gmail.com>
2024-05-19 15:44:45 -0600

    ..

--- ui.py
+++ ui.py
@@ -6,26 +6,72 @@ import os
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=20, rows=4, dotsize=8)
 
 menu = ["Pos)   ", "Time)  ", "Alarm) ", "State) "]
-state= ["Pending", "Descending", "Ascending"]
+states= ["Pending", "Descending", "Ascending"]
 os.environ['TZ'] = 'America/Denver'
 time.tzset()
+alarm = "05:30:00"
+pos_prev = 0
 
+state_index = 0
+state=states[0]
+
+def state_next():
+    state_index = ((state_index+1) % len(states))
+    state= states[state_index]
 
 def get_pos():
     return 0
 
-def update_menu():
-    c = datetime.now()
+def rise_n_shine(date_string):
+    alarm_split = alarm.split(':')
+    time_split = date_string.split(':')
 
-    menu[0] = "Pos   ) " +  str(get_pos())
-    menu[1] = "Time  ) " + c.strftime('%H:%M:%S')
-    menu[2] = "Alarm ) " + "OFF"
-    menu[3] = "State ) " + "OFF"
+    hour_equal = (alarm_split[0] == time_split[0])
+    minute_equal = (alarm_split[1] == time_split[1])
+    second_greater = (alarm_split[2] >= time_split[2])
+
+    return hour_equal and minute_equal and second_greater
+
+def send_ascend():
+    return 0
+
+def send_descend():
+    return 0
+
+def update():
+    c = datetime.now()
+    date_string = c.strftime('%H:%M:%S')
+    pos_prev = pos
+    pos = get_pos()
+
+    if state == states[0]:
+        if rise_n_shine(date_string):
+            lcd.clear()
+            menu[0] = "Pos   ) " +  str(pos)
+            menu[1] = "Time  ) " + date_string
+            menu[2] = "Alarm ) " + alarm
+            menu[3] = "State ) " + "Triggered"
+            time.sleep(60)
+            send_descend()
+            state_next()
+    elif state == states[1]:
+        if pos == pos_prev:
+            send_ascend()
+            state_next()
+    elif state == states[2]:
+        if pos == pos_prev:
+            state_next()
+            
+    lcd.clear()
+    menu[0] = "Pos   ) " +  str(pos)
+    menu[1] = "Time  ) " + date_string
+    menu[2] = "Alarm ) " + alarm
+    menu[3] = "State ) " + state
 
 while 1:
-    time.sleep(1)
+    time.sleep(7)
+    update()
     lcd.clear()
-    update_menu()
     for s in menu:
         pad = ' ' * (20 - len(s)) 
         lcd.write_string(s + pad)
